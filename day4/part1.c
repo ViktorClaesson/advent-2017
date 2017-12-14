03c04b5e1f080077c42c3268ff57e72378e5d398
Viktor Claesson <claesson.viktor@gmail.com>
2017-12-14 14:47:08 +0100

    day 4 part 1

---
+++ day4/part1.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int process_line(char *line) {
+  char *line_copy = line;
+
+  int spaces = 0;
+  int len = strlen(line_copy);
+  for(int i = 0; i < len; i++) {
+    if(line_copy[i] == ' ')
+      spaces += 1;
+  }
+
+  char *words[spaces+1];
+  char *current_word;
+
+  int len2 = sizeof(words) / sizeof(*words);
+
+  for(int i = 0; i < len2; i++) {
+    words[i] = strsep(&line_copy, " \n");
+  }
+
+  for(int i = 0; i < len2; i++) {
+    for(int j = i+1; j < len2; j++) {
+      if(strcmp(words[i], words[j]) == 0) {
+          return 0;
+      }
+    }
+  }
+
+  return 1;
+}
+
+int main(int argc, char * argv[]) {
+  FILE *fp;
+  if((fp = fopen("./input", "r")) == NULL) {
+    perror("error reading file");
+    return 1;
+  }
+
+  size_t size = sizeof(char);
+  char *line = malloc(size);
+  int valid = 0;
+  while(getline(&line, &size, fp) != -1) {
+    valid += process_line(line);
+  }
+
+  if(line) {
+    free(line);
+    line = NULL;
+  }
+
+  printf("There are %d valid lines\n", valid);
+  return 0;
+}
