22650e3e1c00d78c340c3e1897aef97174aceae3
Viktor Claesson <claesson.viktor@gmail.com>
2017-12-14 16:47:13 +0100

    day 5 part 1

---
+++ day5/part1.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct point {
+  int value;
+  struct point *next;
+  struct point *prev;
+};
+
+int main(int argc, char *argv[]) {
+  FILE *fp;
+  if((fp = fopen("./input", "r")) == NULL) {
+    perror("error opening file");
+    return 1;
+  }
+
+  // INIT
+  size_t size = 0;
+  char *line = NULL;
+  struct point start = {0, NULL, NULL};
+  if(getline(&line, &size, fp) != -1) {
+    start.value = atoi(line);
+  }
+
+  struct point *curr = &start;
+  struct point *next;
+  while(getline(&line, &size, fp) != -1) {
+    next = malloc(sizeof(struct point));
+    next->value = atoi(line);
+    next->prev = curr;
+    curr->next = next;
+    curr = next;
+  }
+  free(line);
+
+  // SOLVE PUZZLE
+  struct point *position = &start;
+  int moves = 0;
+  while(position != NULL) {
+    moves += 1;
+    int steps = position->value;
+    position->value+=1;
+    while(steps > 0) {
+      if(position == NULL) break;
+      position = (position->next);
+      steps -= 1;
+    }
+    while(steps < 0) {
+      if(position == NULL) break;
+      position = (position->prev);
+      steps += 1;
+    }
+  }
+
+  printf("It took %d moves\n", moves);
+
+  if(fclose(fp)) {
+    perror("error closing file");
+    return 1;
+  }
+}
